6e9f0fd97df1a58dcac943ce7fcfd5804b46701a
sandeep8854 <sandeepstructure@gmail.com>
2021-06-09 15:01:06 +0530

    Watermelon...

---
+++ DS-And-Algorithms-Program/src/Watermelon_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Watermelon_1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Any Weight of Watermelon  ?   ? ");
+		int wm=sc.nextInt();
+		watermelon(wm);	
+	}
+	private static void watermelon(int wm)
+	{
+		if(wm>2)
+		{
+			if((wm%2)==0)
+				System.out.println("YES");
+			else
+				System.out.println("NO");
+		}
+		else
+		{
+			System.out.println("NO");
+		}
+	}	
+}
