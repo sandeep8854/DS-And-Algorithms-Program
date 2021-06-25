40cc1aa2d157b0d95db5ac98d0f9f66dad10aea2
sandeep8854 <sandeepstructure@gmail.com>
2021-06-25 10:00:06 +0530

    Number mirror.

---
+++ DS-And-Algorithms-Program/src/sandeep/codechef/com/CodeChef_1_Number_Mirror.java
@@ -0,0 +1,24 @@
+package sandeep.codechef.com;
+
+import java.util.Scanner;
+
+public class CodeChef_1_Number_Mirror 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Any Number ?  ? ");
+		int number=sc.nextInt();
+		int res=numberMirror(number);
+		System.out.println(res);
+	}
+	private static int numberMirror(int number)
+	{
+		return number;
+	}
+}
+/*
+Enter Any Number ?  ? 
+123
+123
+*/
